1b6ee9f1285169f0c98a0414037798bfdb57ef40
Pham Tie Hung <hungptse183180@fpt.edu.vn>
2025-02-26 00:55:40 +0700

    nothing

--- obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf927d1dddd6da6bce2197da31af47e368cb9850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2baafed79ecfe934d80e0bf02d55958c67051467")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
